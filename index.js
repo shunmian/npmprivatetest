fcb9045e2494ab0e843cb12d1b25d676f4eb1824
Shunmian <shunmian@gmail.com>
2018-12-31 11:09:38 +0800

    setup private module

---
+++ index.js
@@ -0,0 +1,11 @@
+const logger = () => {
+  const greetings = 'Hello,local-validatorall!'
+  console.log(greetings)
+  return greetings
+}
+
+console.log('local-validatorall')
+
+module.exports = {
+  logger
+}
